0ea621f76c6bf9ce54ad2b23bbbef4186ebe3e87
Rwik Mukhopadhyay <rwikee@gmail.com>
2022-03-16 22:20:02 +0530

    fixed import statement

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,8 @@
 import chalk from 'chalk';
 import validator from 'validator';
-//import getNotes from 'notes';
-//const msg = getNotes();
-//console.log(msg);
+import getNotes from 'file:///C:/vault/GitHub/Node_experiments/notes-app/notes.js';
+const msg = getNotes();
+console.log(msg);
 
 console.log(chalk.red.inverse(validator.isEmail('abc@gmail.com')));
 
